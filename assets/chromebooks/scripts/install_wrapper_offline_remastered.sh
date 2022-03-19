212581cd791f9f86e0344434bdab17778e993021
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-19 16:06:42 -0400

    Update install_wrapper_offline_remastered.sh

--- assets/chromebooks/scripts/install_wrapper_offline_remastered.sh
+++ assets/chromebooks/scripts/install_wrapper_offline_remastered.sh
@@ -1,3 +1,7 @@
+# Install Wrapper: Offline Remastered
 wget https://github.com/ykranimate2021-ga/WrapperOfflineRemasteredV2/archive/chromebook.zip
 unzip chromebook.zip
 unlink chromebook.zip
+# Install Wrapper: Offline Remastered's Dependicies
+cd WrapperOfflineRemasteredV2-chromebook/wrapper
+npm start
